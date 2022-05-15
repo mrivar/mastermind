2617f32665ef47fbb52646247592342ed57a4514
martin-riva-tg <martin.delariva@taiger.com>
2022-05-15 18:30:48 +0200

    Add guess model

--- game/models.py
+++ game/models.py
@@ -1,6 +1,5 @@
 from django.db.models import Model, CharField
 
 
-# Create your models here.
 class Game(Model):
     code = CharField(max_length=4)
--- guess/models.py
+++ guess/models.py
@@ -1,3 +1,18 @@
-from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db.models import Model, CharField, ForeignKey, CASCADE, IntegerField
 
-# Create your models here.
+from game.models import Game
+
+
+class Guess(Model):
+    code = CharField(max_length=4)
+    game = ForeignKey(to=Game, on_delete=CASCADE, related_name='guesses')
+    white_pins = IntegerField(validators=[MaxValueValidator(4), MinValueValidator(0)])
+    black_pins = IntegerField(validators=[MaxValueValidator(4), MinValueValidator(0)])
+
+    def _compute_pins(self):
+        raise NotImplementedError
+
+    def save(self, *args, **kwargs):
+        self._compute_pins()
+        return super().save(*args, **kwargs)
